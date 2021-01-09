285942358adbbd3b6d736a5eacab991ed57db884
alifba <alif252729@gmail.com>
2021-01-09 17:40:18 +0700

    berhasil memakai model

--- app/Controllers/Siswa.php
+++ app/Controllers/Siswa.php
@@ -4,27 +4,75 @@ namespace App\Controllers;
 
 use App\Models\angkatanModel;
 use App\Models\angkatanjurusanModel;
+use App\Models\jurusanModel;
+use App\Models\kelasModel;
+use App\Models\kelassiswaModel;
+use App\Models\lanjutModel;
+use App\Models\lulusanModel;
+use App\Models\lulusanjurusanModel;
+use App\Models\mutasiModel;
+use App\Models\siswaModel;
+use App\Models\siswalanjutModel;
+use App\Models\statusModel;
+use App\Models\tahunajarModel;
 
 class Siswa extends BaseController
 {
     protected $angkatan;
     protected $angkatanjurusan;
+    protected $jurusan;
+    protected $kelas;
+    protected $kelassiswa;
+    protected $lanjut;
+    protected $lulusan;
+    protected $lulusanjurusan;
+    protected $mutasi;
+    protected $siswa;
+    protected $siswalanjut;
+    protected $status;
+    protected $tahunajar;
+
     public function __construct()
     {
         $this->angkatan = new angkatanModel();
         $this->angkatanjurusan = new angkatanjurusanModel();
+        $this->jurusan = new jurusanModel();
+        $this->kelas = new kelasModel();
+        $this->kelassiswa = new kelassiswaModel();
+        $this->lanjut = new lanjutModel();
+        $this->lulusan = new lulusanModel();
+        $this->lulusanjurusan = new lulusanjurusanModel();
+        $this->mutasi = new mutasiModel();
+        $this->siswa = new siswaModel();
+        $this->siswalanjut = new siswalanjutModel();
+        $this->status = new statusModel();
+        $this->tahunajar = new tahunajarModel();
     }
 
     public function index()
     {
-        return view('Siswa/index');
+        $tahunajar = $this->tahunajar->findAll();
+        $kelas = $this->kelas->findAll();
+        $jurusan = $this->jurusan->findAll();
+        $siswa = $this->siswa->findAll();
+
+        $data = [
+            'siswa' => $siswa,
+            'kelas' => $kelas,
+            'jurusan' => $jurusan,
+            'tahunajar' => $tahunajar,
+            'title' => 'Daftar siswa'
+        ];
+        return view('Siswa/index', $data);
     }
 
     public function coba()
     {
-        $siswa = $this->angkatan->findAll();
+        $angkatan = $this->angkatan->findAll();
+
+
         $data = [
-            'siswa' => $siswa,
+            'angkatan' => $angkatan,
             'title' => 'coba'
         ];
         return view('coba', $data);
--- app/Views/Siswa/index.php
+++ app/Views/Siswa/index.php
@@ -1,7 +1,6 @@
 <?= $this->extend('layout/template') ?>
 <?= $this->section('content') ?>
 
-
 <h3><i class="fas fa-user-graduate mr-2"></i> DAFTAR SISWA</h3>
 <hr>
 
@@ -14,7 +13,10 @@
     <div class="col-sm-7">
         <form action="" method="POST">
             <select class="form-control">
-                <option>Tahun Ajaran</option>
+                <option>Pilih Tahun Ajaran</option>
+                <?php foreach ($tahunajar as $a) : ?>
+                    <option value="<?= $a['NM_THNAJAR']; ?>" name="Model"><?= $a['NM_THNAJAR']; ?></option>
+                <?php endforeach; ?>
             </select>
         </form>
     </div>
@@ -24,9 +26,14 @@
         <h5><i class="fas fa-angle-right"></i> Jurusan :</h5>
     </div>
     <div class="col-sm-7">
-        <select class="form-control">
-            <option>Jurusan</option>
-        </select>
+        <form action="" method="POST">
+            <select class="form-control">
+                <option>Pilih Jurusan</option>
+                <?php foreach ($jurusan as $j) : ?>
+                    <option><?= $j['NM_JURUSAN']; ?></option>
+                <?php endforeach; ?>
+            </select>
+        </form>
     </div>
 </div>
 <div class="row mb-1">
@@ -34,9 +41,14 @@
         <h5><i class="fas fa-angle-right"></i> Kelas :</h5>
     </div>
     <div class="col-sm-7">
-        <select class="form-control">
-            <option>Kelas</option>
-        </select>
+        <form action="" method="POST">
+            <select class="form-control">
+                <option>Pilih Kelas</option>
+                <?php foreach ($kelas as $k) : ?>
+                    <option value="<?= $k['NM_KELAS']; ?>"><?= $k['NM_KELAS']; ?></option>
+                <?php endforeach; ?>
+            </select>
+        </form>
     </div>
 </div>
 
@@ -69,12 +81,13 @@
             </tr>
         </thead>
         <tbody>
-            <?php for ($i = 0; $i <= 10; $i++) : ?>
+            <?php $i = 1 ?>
+            <?php foreach ($siswa as $s) : ?>
                 <tr>
-                    <td>1</td>
+                    <td><?= $i++; ?></td>
                     <td>2020/2021</td>
                     <td>RPL</td>
-                    <td>Alif Nurrohman</td>
+                    <td><?= $s['NM_SISWA']; ?></td>
                     <td>098766</td>
                     <td>19282736</td>
                     <td>
@@ -83,7 +96,7 @@
                         <a href=""><i class="fas fa-trash-alt"></i>
                     </td>
                 </tr>
-            <?php endfor; ?>
+            <?php endforeach; ?>
         </tbody>
     </table>
 </div>
