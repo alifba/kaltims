2d21d5cf093c6e51054914ebd4cdf6ee8cec8c3c
alifba <alif252729@gmail.com>
2021-02-01 00:27:57 +0700

    sorry tadi nyoba revert coy

--- app/Views/layout/template.php
+++ app/Views/layout/template.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="fontawesome-free-5.15.1-web/css/all.min.css">
+    <link rel="stylesheet" href="/fontawesome-free-5.15.1-web/css/all.min.css">
     <link rel="stylesheet" href="/css/bootstrap.min.css">
     <link rel="stylesheet" href="/css/admin.css">
 
